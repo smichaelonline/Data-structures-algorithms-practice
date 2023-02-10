ce252f8373618cf58b0d429bc89579f8ec0728b8
smichaelonline <stephaniemichael001@gmail.com>
2023-02-10 12:11:02 -0500

    Create new example of breaking down a problem

--- Practice-and-examples/problem-solving.js
+++ Practice-and-examples/problem-solving.js
@@ -1,8 +1,16 @@
 // write a function which takes two numbers and returns their sum? 
 /* 
 ? Can I restate the problem in my own words?
+  - We have to write a function that adds two numbers
 ? What are the inputs that go into the problem?
+  !ask yourself!: How large are the numbers going to be?
+  - Int? 
+  - Float? 
+  - String for large numbers?
 ? What are the outputs that should come from the solution to the problem? 
+  - Total of the two inputs but will we be using Int? Float? String? 
 ? Can the outputs be determined from the inputs? Do I have enough information to solve the problem? 
+  - What if the user only inputs one number? We would not have enough information to run the algorithm. *good point to ask interviewer
 ? How should I label the important pieces of data that are a part of the problem? 
+  - Labels will be num 1 and num 2 as the arguments. the result of the algoritm in our return would be sum. 
 */
